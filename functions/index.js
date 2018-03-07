9ae9794ba8f7bae01dccfc4fc39b6e102d6c2cff
mafergus <matthew.n.ferguson@gmail.com>
2018-03-07 02:20:22 -0800

    Adding some stuffs

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,91 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+// Twilio Credentials
+const accountSid = 'ACa2c223eafd99f3f430296ea346ea63ec';
+const authToken = '4670e436d6461e314cfe2e38102940f7';
+// require the Twilio module and create a REST client
+const client = require('twilio')(accountSid, authToken);
+
+var moment = require('moment');
+
+function sendMessage(to, body) {
+  return client.messages
+  .create({
+    to,
+    from: '+18316100384',
+    body,
+  })
+  .then(message => console.log(message))
+  .catch(err => console.log(err));
+}
+
+function getMessageBody(quote) {
+  return quote.text + " - " + quote.author + "\n\n" 
+    + "Your daily inspiration from Ywake. Find more inspiration at www.ywake.com";
+}
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  // Twilio Credentials
-  const accountSid = 'ACa2c223eafd99f3f430296ea346ea63ec';
-  const authToken = '4670e436d6461e314cfe2e38102940f7';
-
-  // require the Twilio module and create a REST client
-  const client = require('twilio')(accountSid, authToken);
-
   return client.messages
   .create({
     to: '+16507961513',
     from: '+18316100384',
     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
   })
-  .then(message => console.log(message.sid));
+  .then(message => console.log(message))
+  .catch(err => console.log(err));
+});
+
+exports.hourly_job = functions.pubsub.topic('hourly-tick').onPublish((event) => {
+  var now = moment().utc();
+  var roundDown = now.startOf('hour');
+  console.log("Time now() ", roundDown.hour());
+
+  return admin.database().ref('users')
+  .orderByChild("time")
+  .equalTo(roundDown.hour())
+  .once("value")
+  .then(snapshot => {
+    snapshot.forEach(userData => {
+      const userId = userData.key;
+      const user = userData.val();
+
+      admin.database().ref('quotes/' + user.nextQuote)
+      .once('value')
+      .then(snapshot => {
+        if (snapshot.val() !== null) {
+          const quote = snapshot.val();
+          console.log("Quote: ", quote);
+
+          sendMessage(user.phoneNumber, getMessageBody(quote));
+          return admin.database().ref('users/' + userId + "/nextQuote").set(user.nextQuote+1);
+        }
+        return "Couldn't get a quote!";
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    });
+
+    return
+  })
+  .catch(err => {
+    console.log(err);
+    return;
+  });
+
+  // return admin.database().ref('users/' + userId).once('value')
+  //   .then(userData => { 
+  //     console.log("userData: ", userData.val());
+  //     if (!userData.exists()) {
+  //       const userObject = {
+  //         displayName: getDisplayName(),
+  //         highScore: 0,
+  //       };
+  //       return admin.database().ref('users/' + userId).set(userObject);
+  //     }
+  //     return null;
+  //   })
+  //   .catch(err => console.log(err));
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,63 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+
+const items = [];
+items.push(<MenuItem value={0} key={0} primaryText={`12:00am`} />);
+for (let i=1; i < 12; i++) {
+  items.push(<MenuItem value={i} key={i} primaryText={`${i}:00am`} />);
+}
+items.push(<MenuItem value={12} key={12} primaryText={`12:00pm`} />);
+for (let i=1; i < 12; i++) {
+  items.push(<MenuItem value={i} key={i+12} primaryText={`${i}:00pm`} />);
+}
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+    };
+  }
+
+  handleChange = (event, index, value) => this.setState({ value });
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div className="hero" style={{ width: "100%", height: "100%", display: "flex", flexDirection: "column" }}>
+        <h1 style={{ fontSize: "5em", color: "white", width: "100%", textAlign: "center" }}>
+          YWake
+        </h1>
+        <p style={{ fontSize: "1.5em", color: "white", width: "100%", textAlign: "center" }}>
+          Welcome to Ywake! Sign up for a daily dose of inspirational goodness, sent right to your phone!
         </p>
+        <div style={{ width: 500, padding: 10, marginTop: 200, backgroundColor: "white", textAlign: "center", alignSelf: "center", borderRadius: 5 }}>
+          <TextField
+            style={{ marginTop: 50, width: 350 }}
+            hintText="Enter Your Phone Number, with country code! eg +16508546767"
+          />
+          <DropDownMenu maxHeight={300} value={this.state.value} onChange={this.handleChange}>
+            {items}
+          </DropDownMenu>
+          <RaisedButton label="Let's Go!" />
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+class MaterialApp extends Component {
+  render() {
+    return <MuiThemeProvider>
+      <App />
+    </MuiThemeProvider>;
+  }
+}
+
+export default MaterialApp;
