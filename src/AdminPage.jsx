0bb33128f439400d9eff13a7512c53d4d2df415a
mafergus <matthew.n.ferguson@gmail.com>
2018-03-17 02:22:42 -0700

    Stub admin page

---
+++ src/AdminPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import autoBind from 'react-autobind';
+
+export default class AdminPage extends Component {
+
+  render() {
+    return <div style={{ width: "100%", height: "100%", backgroundColor: "green" }}>
+    </div>;
+  }
+}
